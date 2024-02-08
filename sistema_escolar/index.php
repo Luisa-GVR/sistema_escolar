47aee104f53fc6a0bdb88a8045b2b7a0a4248756
iosaf <116690709+Luisa-GVR@users.noreply.github.com>
2024-02-08 15:09:12 -0700

    Add files via upload

--- sistema_escolar/index.php
+++ sistema_escolar/index.php
@@ -1,9 +1,27 @@
 <!DOCTYPE html>
 <html>
+<style>
+    .form-group {
+        margin-bottom: 15px;
+    }
+
+    .form-group label {
+        display: block;
+        margin-bottom: 5px;
+    }
+
+    .form-group input {
+        padding: 8px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        width: calc(20%);
+    }
+</style>
+
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Sistema Escolar de la Uni</title>
+    <title>Sistema Escolar</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
@@ -11,14 +29,23 @@
 <body>
     <h1>Sistema escolar</h1>
     <form action="validar.php" method="POST">
-        <label for="user"> Matricula:</label>
-        <input type="number" name="user" required>
-        <br>
-        <label for="pswd"> Contraseña:</label>
-        <input type="password" name="pswd" id="" required>
-        <br>
-        <button type="submit">padentro</button>
+        <div class="form-group">
+            <label for="user">Matricula:</label>
+            <input type="number" name="user" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Contraseña:</label>
+            <input type="password" name="password" id="" required>
+        </div>
+        <button type="submit">Submit</button>
     </form>
+    <?php
+
+    //validamos el warning
+    if (!empty($_GET["error"]) && $_GET["error"] == 100) {
+        echo "Hubo un problema en el acceso";
+    }
     
+    ?>
 </body>
 </html>
--- sistema_escolar/validar.php
+++ sistema_escolar/validar.php
@@ -1,18 +1,26 @@
 <?php
+
+if (empty($_POST["user"]) || empty($_POST["password"])) {
+    header  ("Location:index.php");
+}
     print_r ($_POST);
-    echo $_POST["user"];
     echo "<br>";
-    $SQL = "SELECT * FROM alumno where Expediente = " .$_POST["user"] ." AND Contra = MD5('".$_POST["pswd"]."');";
+
+    $SQL = "SELECT * FROM alumno where Expediente = " .$_POST["user"] ." AND Contra = MD5('".$_POST["password"]."');";
+    
     echo $SQL;
-
     echo "<br>";
 
-
-    $connection = mysqli_connect("localhost:3309","root","", "sistema_escolar") or die("La conexión ha fallado");
-    echo "<br>:3";
+    $connection = mysqli_connect("localhost:3307","root","", "sistema_escolar") or die("La conexión ha fallado");
     
     $result = mysqli_query($connection, $SQL) or die("La conexión ha fallado");
-    echo ("<br>:3 <br>"); 
 
-    print_r ($result);
+    if ($result->num_rows == 1){
+        echo "Usuario Correcto";
+        header  ("Location:menu.php");
+    } else{
+        header  ("Location:index.php?error=100");
+    }
+    mysqli_close($connection);
+
 ?>
